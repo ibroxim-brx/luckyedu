b3d674997d5e74299f7e9b65dd01b9384e1b4837
unknown <ibrash.00@mail.ru>
2020-09-15 15:37:27 +0500

    sss

--- contact-form-hundler.php
+++ contact-form-hundler.php
@@ -1,4 +1,30 @@
 <?php
+$name = $_POST['usern-name'];
+$phone = $_POST['user-phone'];
+$email = $_POST['user-email'];
+$token = "1054630785:AAHny5f-iMWFZpI8ZSQhbyxZErCNoqwjOHk";
+$chat_id = "-496692311";
+$arr = array(
+'user name' =>$name,
+'user phone' => $phone,
+'email'=> $email
+);
+
+foreach ($arr as $key => $value) {
+	$txt .= "<b>".$key."<b>".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={chat_id}
+	&parse_mode=html&text={$txt}","r")
+
+if ($sendToTelegram) {
+	header('Location: index.html');
+}
+else {
+	echo "error";
+}
+?>
+<!-- <?php
 $name = $_Post['name'];
 $visitor_email = $_Post['email'];
 $message = $_POST['message'];
@@ -20,4 +46,4 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body, $headers);
 
 header("Location: contact_us.html")
->
+> -->
